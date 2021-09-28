d0cd88d936f642dfc5c0a158ef2d3dce373f96f5
Serega <agentserj@mail.ru>
2021-09-28 17:13:08 +0300

    Lesson-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
@@ -6,10 +6,30 @@ import { Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
+  counter = 0
+  updateAt?: number;
+
   constructor() {}
 
+  get cannotDec(): boolean {
+    return this.counter <= 0
+  }
 
+  inc() {
+    this.updateAt = Date.now()
+    this.counter++
+  }
+
+  dec() {
+    this.updateAt = Date.now()
+    this.counter--
+  }
+
+  clear() {
+    this.updateAt = Date.now()
+    this.counter = 0
+  }
 
 
 }
