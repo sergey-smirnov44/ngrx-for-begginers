f0bccc6558ffbc4c5dc59d6a04c34bce001a2a82
Serega <agentserj@mail.ru>
2021-09-28 16:35:53 +0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC4y8CQpKFmWXHnRUe0gETYR7iEQegDmAU",
+    authDomain: "ng-fitness-tracker-50926.firebaseapp.com",
+    projectId: "ng-fitness-tracker-50926",
+    storageBucket: "ng-fitness-tracker-50926.appspot.com",
+    messagingSenderId: "1011154322668",
+    appId: "1:1011154322668:web:0d5b212fc3b1ba058151c9",
+    measurementId: "G-FBBPSYZQYH"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC4y8CQpKFmWXHnRUe0gETYR7iEQegDmAU",
+    authDomain: "ng-fitness-tracker-50926.firebaseapp.com",
+    projectId: "ng-fitness-tracker-50926",
+    storageBucket: "ng-fitness-tracker-50926.appspot.com",
+    messagingSenderId: "1011154322668",
+    appId: "1:1011154322668:web:0d5b212fc3b1ba058151c9",
+    measurementId: "G-FBBPSYZQYH"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
